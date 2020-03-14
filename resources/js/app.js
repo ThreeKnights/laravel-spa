d9d715a5ae579818bc242409bf594739ae3593a5
abhishekpakhare97 <abhishekpakhare1997@gmail.com>
2020-03-14 07:32:00 +0530

    [frontEnd] : Vue components create and Vue router is set up

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,52 @@ window.Axios = require('axios').default;
 
 let AppLayout = require('./components/App.vue');
 
+// Show the list of post 
+const Listpost = Vue.component('Listposts', require('./components/Listpost.vue'));
+
+// add post template
+const Addpost = Vue.component('Addpost', require('./components/Addpost.vue'));
+
+//edit post template 
+const Editpost = Vue.component('Editpost', require('./components/Editpost.vue'));
+
+// delete post template 
+const Deletepost = Vue.component('Deletepost', require('./components/Deletepost.vue'));
+
+// view single post 
+const Viewpost = Vue.component('Viewpost', require('./components/Viewpost.vue'));
+
+
+
 Vue.use(VueRouter, VueAxios, Axios);
+
+const routes = [
+    {
+        name : 'Listpost',
+        path : '/',
+        component : Listpost
+    },
+    {
+        name : 'Addpost',
+        path : '/add-post',
+        component : Addpost
+    },
+    {
+        name : 'Editpost',
+        path : '/edit/:id',
+        component : Editpost
+    },
+    {
+        name : 'Deletepost',
+        path : '/post-delete',
+        component : Deletepost
+    },
+    {
+        name : 'Viewpost',
+        path : '/view/:id',
+        component : Viewpost
+    }
+];
 
 const router = new VueRouter({
     mode : history,
---
+++ resources/js/components/Listpost.vue
@@ -0,0 +1,17 @@
+<template id="post-list">
+    <div class="row">
+        <div class="pull-right">
+            <router-link class="btn btn-xs btn-primary">
+                Add post    
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
