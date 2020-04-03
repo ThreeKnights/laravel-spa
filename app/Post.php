f230df1ae7238c0e76ade386c0faf78d973c4372
abhishekpakhare97 <abhishekpakhare1997@gmail.com>
2020-04-03 21:39:17 +0530

    [Back End] : comment and model created

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $table = 'posts';
+    // protected $table = 'posts';
 
     protected $fillable = ['title','body'];
+
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
---
+++ database/migrations/2020_04_03_154206_rename_blog_comments_to_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameBlogCommentsToCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('blog_comments', 'comments');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('blog_comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
